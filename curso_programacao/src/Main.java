379d04eeb8efd2edb161674d48afd738762e8d7d
Caio César <caiogalaxy00@gmail.com>
2024-07-13 11:10:46 -0300

    entrada de dados

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -25,24 +25,6 @@ public class Main {
 		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
 		Locale.setDefault(Locale.US);
 		System.out.printf("US decimal point: %.3f%n", measure);
-
-
-		
-		
-		
-		/*
-		double x = 10.35784;
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n", x);
-		System.out.println("RESULTADO = " + x + " METROS");
-		System.out.printf("RESULTADO = %.2f metros%n", x);
-		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
-		*/
 	}
 
 }
