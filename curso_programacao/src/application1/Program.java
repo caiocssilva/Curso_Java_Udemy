3bdfd896c10a4e181f6b34e223b882b6f9bf7d52
Caio César <caiogalaxy00@gmail.com>
2024-07-30 11:10:59 -0300

    melhorias

--- curso_programacao/src/application1/Program.java
+++ curso_programacao/src/application1/Program.java
@@ -13,11 +13,14 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		Product product = new Product();
+		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		product.name = sc.nextLine();
+		
 		System.out.print("Price: ");
 		product.price = sc.nextDouble();
+		
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
