5fb63177308a06bcf77f08f9ca55183691156953
Caio César <caiogalaxy00@gmail.com>
2024-07-30 11:32:50 -0300

    this

--- curso_programacao/src/application1/Program.java
+++ curso_programacao/src/application1/Program.java
@@ -28,7 +28,7 @@ public class Program {
 		System.out.println("Product data: " + product);
 		
 		System.out.println();
-		System.out.println("Enter the number of products to be added in stock: ");
+		System.out.print("Enter the number of products to be added in stock: ");
 		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
@@ -36,7 +36,7 @@ public class Program {
 		System.out.println("Updated data: " + product);
 		
 		System.out.println();
-		System.out.println("Enter the number of products to be removed from stock: ");
+		System.out.print("Enter the number of products to be removed from stock: ");
 		quantity = sc.nextInt();
 		product.removeProducts(quantity);
 		
