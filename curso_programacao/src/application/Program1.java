3d53d9844a86041118921c84fc36acc012d07ae4
Caio César <caiogalaxy00@gmail.com>
2024-07-31 10:10:07 -0300

    exercicio

--- curso_programacao/src/application/Program1.java
+++ curso_programacao/src/application/Program1.java
@@ -13,29 +13,44 @@ public class Program1 {
 		Scanner sc = new Scanner(System.in);
 		Account account;
 		
-		System.out.println("Enter account number: ");
+		System.out.print("Enter account number: ");
 		int number = sc.nextInt();
 		
-		System.out.println("Enter account holder: ");
-		sc.nextLine();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();										// quebra de linha pendente
 		String holder = sc.nextLine();
 		
-		System.out.println("Is there na initial deposit (y/n)? ");
+		System.out.print("Is there na initial deposit (y/n)? ");
 		char response = sc.next().charAt(0);
 		
 		if (response == 'y') {
-			System.out.println("Enter initial deposit value: ");
+			System.out.print("Enter initial deposit value: ");
 			double initialDeposit = sc.nextDouble();
 			account = new Account(number, holder, initialDeposit);
 		}
 		else {
 			account = new Account(number, holder);
 		}
-			
 		
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(account);
 		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		Double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+				
 		sc.close();
-		
 		
 	}
 
