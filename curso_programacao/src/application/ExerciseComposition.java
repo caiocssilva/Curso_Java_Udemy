1f8c2ee7cb4179d258746e79d035bfef4bac4d3a
Caio César <caiogalaxy00@gmail.com>
2024-09-03 10:29:14 -0300

    Heranca

--- curso_programacao/src/application/ExerciseComposition.java
+++ curso_programacao/src/application/ExerciseComposition.java
@@ -7,8 +7,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Client;
+import entities.OrderEx;
 import entities.OrderItem;
 import entities.Product;
+import entities.enums.OrderStatus;
 
 public class ExerciseComposition {
 
@@ -24,12 +26,18 @@ public class ExerciseComposition {
 		String name = sc.nextLine();
 		
 		System.out.print("Email: ");
-		String email = sc.nextLine();
+		String email = sc.next();
 		
 		System.out.print("Birth date (DD/MM/YYYY): ");
 		Date birthDate = sdf.parse(sc.next());
 		
 		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		OrderEx order = new OrderEx(new Date(), status, client);
 		
 		System.out.print("How many items to this order? ");
 		int n = sc.nextInt();
