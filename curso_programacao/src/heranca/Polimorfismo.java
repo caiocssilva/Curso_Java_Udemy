06511267ed4ad4a5734bb3f2bf672976855a7c13
Caio César <caiogalaxy00@gmail.com>
2024-09-04 10:12:18 -0300

    Polimorfismo

---
+++ curso_programacao/src/heranca/Polimorfismo.java
@@ -0,0 +1,18 @@
+package heranca;
+
+public class Polimorfismo {
+
+	public static void main(String[] args) {
+
+		Account x = new Account(1020, "Caio", 1000.0);
+		Account y = new SavingsAccount(1023, "Mara", 1000.0, 0.01);
+		
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+
+	}
+
+}
